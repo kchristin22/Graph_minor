e3898aade1ead95f54c126a54a0703571249547e
kchristin22 <christinakoutsou22@gmail.com>
2023-11-11 00:45:23 +0200

    Add CSR compression

--- inc/sequential.hpp
+++ inc/sequential.hpp
@@ -3,7 +3,11 @@
 #include "vector"
 #include "cstddef"
 
-void seqDense(std::vector<int> &M, size_t *L, std::vector<int> &A, std::vector<size_t> &c); // mixture (to be deleted after testing)
+void seqDense(std::vector<int> &M, std::vector<int> &A, std::vector<size_t> &c); // mixture (to be deleted after testing)
 
 /* Dense matrix implementation */
-void seq(std::vector<int> &M, size_t *L, std::vector<int> &A, std::vector<size_t> &c);
+void seq(std::vector<int> &M, std::vector<int> &A, std::vector<size_t> &c);
+
+/* CSR matrix implementation */
+void seq(std::vector<size_t> &rowM, std::vector<size_t> &colM, std::vector<int> &valM,
+         std::vector<size_t> &row, std::vector<size_t> &col, std::vector<int> &val, std::vector<size_t> &c);
--- src/sequential.cpp
+++ src/sequential.cpp
@@ -2,7 +2,7 @@
 #include "stdio.h"
 #include "cstdlib"
 
-void seqDense(std::vector<int> &M, size_t *L, std::vector<int> &A, std::vector<size_t> &c)
+void seqDense(std::vector<int> &M, std::vector<int> &A, std::vector<size_t> &c)
 {
     printf("Hello from seq\n");
 
@@ -49,7 +49,6 @@ void seqDense(std::vector<int> &M, size_t *L, std::vector<int> &A, std::vector<s
             continue;
         nclus += 1;
     }
-    *L = nclus;
 
     size_t end;
     std::vector<int> colCompressed(n * nclus, 0);
@@ -85,7 +84,7 @@ void seqDense(std::vector<int> &M, size_t *L, std::vector<int> &A, std::vector<s
     }
 };
 
-void seq(std::vector<int> &M, size_t *L, std::vector<int> &A, std::vector<size_t> &c)
+void seq(std::vector<int> &M, std::vector<int> &A, std::vector<size_t> &c)
 {
     printf("Hello from seq\n");
 
@@ -111,7 +110,6 @@ void seq(std::vector<int> &M, size_t *L, std::vector<int> &A, std::vector<size_t
             continue;
         nclus += 1;
     }
-    *L = nclus;
 
     int val;
     std::vector<int> colCompressed(n * nclus, 0);
@@ -129,10 +127,10 @@ void seq(std::vector<int> &M, size_t *L, std::vector<int> &A, std::vector<size_t
 
     // if ids are continuous they don't need sorting
 
-    M.resize(nclus * nclus);
+    M.resize(nclus * nclus, 0);
 
     for (size_t id = 1; id < (nclus + 1); id++) // cluster ids start from 1
-    {
+    {                                           // access one row of M at a time
         for (size_t i = 0; i < n; i++)
         {
             if (id != c[i]) // c[i]: cluster of row i of colCompressed/row
@@ -145,3 +143,94 @@ void seq(std::vector<int> &M, size_t *L, std::vector<int> &A, std::vector<size_t
         }
     }
 };
+
+void seq(std::vector<size_t> &rowM, std::vector<size_t> &colM, std::vector<int> &valM, std::vector<size_t> &row, std::vector<size_t> &col, std::vector<int> &val, std::vector<size_t> &c)
+{
+    printf("Hello from seq\n");
+
+    if (row.size() != c.size())
+    {
+        printf("Error: sizes of row and c are incompatible\n");
+        exit(1);
+    }
+    else if (col.size() != val.size())
+    {
+        printf("Error: sizes of col and val are incompatible\n");
+        exit(1);
+    }
+    else if (row.size() == col.size())
+    {
+        printf("Error: CSR requires more space than dense matrix representation \n Use dense matrix implementation instead...\n");
+        exit(1);
+    }
+    else if ((rowM.size() != row.size()) || (colM.size() != col.size()) || (valM.size() != val.size()))
+    // if the input graph is its the minor, the dimensions of the compressed vectors should be equal to the dimensions of the input vectors
+    {
+        printf("Error: at least one of the compressed vectors doesn't have enough allocated space \n");
+        exit(1);
+    }
+
+    size_t n = c.size();
+    size_t nz = val.size();
+
+    std::vector<size_t> discreetClus(n, 0); // vector where the ith element is a if cluster i has a nodes
+
+    for (size_t i = 0; i < n; i++)
+    {
+        discreetClus[c[i] - 1] += 1; // we assume that there is no ith row and column that are both zero so we know that all ids included in c exist in A
+                                     // by atomically adding 1 to the cluster of the ith row we know how many nodes are in each cluster
+    }
+
+    size_t nclus = 0;
+    for (size_t i = 0; i < n; i++)
+    {
+        if (discreetClus[i] == 0)
+            continue;
+        nclus += 1;
+    }
+
+    size_t end, clusterHasElements, allCount = 0; // store offset to assign to each rowM element
+    std::vector<int> auxValueVector(nclus);       // auxiliary vector that will contain all the non-zero values of each cluster (element of rowM)
+    rowM.resize(nclus);                           // resize vector to the number of clusters
+
+    for (size_t id = 1; id < (nclus + 1); id++) // cluster ids start from 1
+    {
+        rowM[id - 1] = allCount;
+        auxValueVector.assign(nclus, 0);
+        clusterHasElements = 0;
+
+        for (size_t i = 0; i < n; i++)
+        {
+            if (id != c[i]) // c[i]: cluster of row i of colCompressed/row
+                continue;
+
+            if (i == (n - 1))
+                end = nz;
+            else
+                end = row[i + 1];
+
+            if (row[i] == end)
+                continue;
+            if (!clusterHasElements) // check vector functions for this
+                clusterHasElements = 1;
+
+            for (size_t j = row[i]; j < end; j++)
+            {
+                auxValueVector[c[col[j]] - 1] += val[j]; // compress cols by summing the values of each cluster to the column the cluster id points to
+            }
+        }
+        if (!clusterHasElements)
+            continue;
+        for (size_t i = 0; i < nclus; i++)
+        {
+            if (auxValueVector[i] == 0)
+                continue;
+
+            valM[allCount] = auxValueVector[i];
+            colM[allCount] = i;
+            allCount++;
+        }
+    }
+    colM.resize(allCount);
+    valM.resize(allCount);
+}
